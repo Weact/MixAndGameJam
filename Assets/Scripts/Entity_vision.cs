5004cb897e5f16b326a1a5b0b4a8e67252345302
Romainmoschler <44899582+Romainmoschler@users.noreply.github.com>
2020-11-14 23:51:16 +0100

    Adding_Vision_of_Entity

---
+++ Assets/Scripts/Entity_vision.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity_vision : MonoBehaviour
+{
+    public Transform player;
+    public float fviewRadius;
+    public float fangleInfo;
+
+    private bool isInvision = false;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, fviewRadius);
+    }
+
+    public static bool Invision(Transform checkingObject, Transform target, float fviewRadius)
+    {
+        bool verif = false;
+        float distanceToEntity = Vector3.Distance(checkingObject.position, target.position);
+        if(distanceToEntity <= fviewRadius)
+        {
+            verif = true;
+        }
+
+      return verif;
+    }
+
+
+    private void Update()
+    {
+        isInvision = Invision(transform, player, fviewRadius);
+
+        if(isInvision)
+        {
+            Debug.Log("In vision");
+        }
+        else
+        {
+            Debug.Log("Not IN vision");
+        }
+    }
+}
+
+    
