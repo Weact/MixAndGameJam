78c6b4654556b93fee741833c6904472d795b82a
ValentinZeller <v.zeller@ludus-academie.com>
2020-11-14 23:59:44 +0100

    Added racing task and the object to start the task

---
+++ Assets/Scripts/RacingTask/RacingTask.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RacingTask : MonoBehaviour
+{
+    public Transform playerTransform;
+    private float timer = 0;
+    private float maxTime = 30;
+    public Text timeLeft;
+
+    private void OnEnable()
+    {
+        Reset();
+    }
+
+    private void OnDisable()
+    {
+        Reset();
+    }
+
+    private void Reset()
+    {
+        timer = 0;
+        playerTransform.SetPositionAndRotation(new Vector3(-94+446.875f, 146+238.125f, 0), Quaternion.Euler(0, 0, 90));
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        timeLeft.text = (Mathf.Round(maxTime - timer)).ToString();
+        if (timer > maxTime)
+        {
+            GameObject.Find("PresentoirRacing").GetComponent<RacingManager>().Interact();
+        }
+    }
+
+    private void OnTriggerEnter2D()
+    {
+        GameObject.Find("PresentoirRacing").GetComponent<RacingManager>().Interact();
+    }
+
+}
