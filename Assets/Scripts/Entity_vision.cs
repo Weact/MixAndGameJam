6a7cb0562954517651b2f2d3f34ef2f3f69efc1e
Romainmoschler <44899582+Romainmoschler@users.noreply.github.com>
2020-11-15 10:47:17 +0100

    Adding_forward_FOV

--- Assets/Scripts/Entity_vision.cs
+++ Assets/Scripts/Entity_vision.cs
@@ -14,15 +14,33 @@ public class Entity_vision : MonoBehaviour
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, fviewRadius);
+
+        Vector3 fovline1 = Quaternion.AngleAxis(fangleInfo, transform.up) * transform.forward * fviewRadius;
+        Vector3 fovline2 = Quaternion.AngleAxis(-fangleInfo, transform.up) * transform.forward * fviewRadius;
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawRay(transform.position, fovline1);
+        Gizmos.DrawRay(transform.position, fovline2);
+
+        Gizmos.color = Color.red;
+
+        float fanglePlayer = Mathf.Atan2(player.position.x-transform.position.x, player.position.z-transform.position.x) * Mathf.Rad2Deg;
+
+        Gizmos.DrawRay(transform.position, Quaternion.AngleAxis(fanglePlayer, transform.up) * transform.forward * fviewRadius);
+
     }
 
-    public static bool Invision(Transform checkingObject, Transform target, float fviewRadius)
+    public static bool Invision(Transform checkingObject, Transform target, float fviewRadius,float fangleInfo)
     {
         bool verif = false;
         float distanceToEntity = Vector3.Distance(checkingObject.position, target.position);
         if(distanceToEntity <= fviewRadius)
         {
-            verif = true;
+            float fanglePlayer = Mathf.Atan2(target.position.x -checkingObject.position.x, target.position.z - checkingObject.position.z) * Mathf.Rad2Deg;
+            if (fanglePlayer <= fangleInfo && fanglePlayer >= -fangleInfo)
+            {
+                verif = true;
+            }
         }
 
       return verif;
@@ -31,7 +49,7 @@ public class Entity_vision : MonoBehaviour
 
     private void Update()
     {
-        isInvision = Invision(transform, player, fviewRadius);
+        isInvision = Invision(transform, player, fviewRadius,fangleInfo);
 
         if(isInvision)
         {
