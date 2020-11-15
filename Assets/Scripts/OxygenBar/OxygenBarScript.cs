ca389b979f625308bfd21e953ef5f94997fcf637
Weact <l.druckes@ludus-academie.com>
2020-11-15 12:34:08 +0100

    oxygen bar, still have to add the task function

---
+++ Assets/Scripts/OxygenBar/OxygenBarScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OxygenBarScript : MonoBehaviour
+{
+    public int normal_reduction = 1;
+
+    [SerializeField]
+    private float timeRate = 1; //In second
+
+    private Slider OxygenBar;
+    private Text OxygenTextDisplay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        OxygenBar = GetComponent<Slider>();
+        OxygenTextDisplay = GetComponent<Text>();
+
+        InvokeRepeating("SubstractPlayerOxygen", timeRate, timeRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OxygenTextDisplay.text = OxygenBar.value.ToString();
+    }
+
+    void SubstractPlayerOxygen()
+    {
+        if(OxygenBar.value > 0)
+        {
+            OxygenBar.value -= normal_reduction;
+        }
+        else
+        {
+            Debug.Log("Player's Oxygen has reached 0 !");
+            CancelInvoke("substractPlayerOxygen");
+        }
+    }
+}
