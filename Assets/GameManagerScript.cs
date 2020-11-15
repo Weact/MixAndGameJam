3aadee3888e1256bc13c5fa7c050d9c950b949af
Romain SCHLOTTER <romain.schlotter@gmail.com>
2020-11-15 17:19:56 +0100

    GameManager et Conditions de victoire

---
+++ Assets/GameManagerScript.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManagerScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Fin = null;
+    [SerializeField]
+    private GameObject Victoire = null;
+
+    private Text AfficheTaches = null;
+
+    public bool bTache1 = false;
+    public bool bTache2 = false;
+    public bool bTache3 = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AfficheTaches = GameObject.Find("TexteSpawn").GetComponent<Text>();
+    }
+
+    public void ValideTache(bool bTache)
+    {
+        bTache = true;
+        AfficheNbTache();
+    }
+
+    public void AfficheNbTache()
+    {
+        string Affiche = CompteBool().ToString() + "/3";
+        AfficheTaches.text = Affiche;
+    }
+
+    private int CompteBool()
+    {
+        int n = 0;
+        if (bTache1)
+            n++;
+        if (bTache2)
+            n++;
+        if (bTache3)
+            n++;
+        return n;
+    }
+
+    public void GameOver()
+    {
+        if (bTache1 && bTache2 && bTache3)
+        {
+            Victory();
+        }
+        else
+        {
+            Defeat();
+        }
+    }
+
+    public void Defeat()
+    {
+        if (Fin != null)
+            Fin.SetActive(true);
+    }
+
+    private void Victory()
+    {
+        if (Victoire != null)
+            Victoire.SetActive(true);
+    }
+}
--- Assets/Scripts/OxygenBar/OxygenBarScript.cs
+++ Assets/Scripts/OxygenBar/OxygenBarScript.cs
@@ -8,19 +8,20 @@ public class OxygenBarScript : MonoBehaviour
     public int normal_reduction = 1;
 
     [SerializeField]
+    private GameManagerScript GMScript = null;
+
+    [SerializeField]
     private float timeRate = 1; //In second
 
     private Slider OxygenBar;
     private Text OxygenTextDisplay;
-
-    [SerializeField]
-    private GameObject Fin = null;
 
     // Start is called before the first frame update
     void Start()
     {
         OxygenBar = GetComponent<Slider>();
         OxygenTextDisplay = GetComponent<Text>();
+        GMScript = GameObject.Find("GameManager").GetComponent<GameManagerScript>();
     }
     
     public void Begin()
@@ -44,8 +45,8 @@ public class OxygenBarScript : MonoBehaviour
         {
             Debug.Log("Player's Oxygen has reached 0 !");
             CancelInvoke("substractPlayerOxygen");
-            if (Fin != null)
-                Fin.SetActive(true);
+            if (GMScript != null)
+                GMScript.Defeat();
         }
     }
 }
